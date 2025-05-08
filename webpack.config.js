ba2eb2bbf887b76635f4f180550d729ceda69eed
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-05-08 12:32:04 -0300

    add: logo

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,24 @@
+const modoDev = process.env.NODE_ENV != 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
     },
     plugins: [
         new MiniCssExtractPlugin({
@@ -22,6 +34,9 @@ module.exports = {
                 'css-loader', // interpreta @import, url()... 
                 'sass-loader',
             ]
+        }, {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: ['file-loader']
         }]
     }
 
