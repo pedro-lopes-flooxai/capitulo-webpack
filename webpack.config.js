b5573e03c8fe0e22f52b205c3fbbe1456ef8daad
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-05-08 11:05:58 -0300

    add: css

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,15 @@ module.exports = {
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                'style-loader', // adiciona CSS DOM injetando a tag <style>
+                'css-loader' // interpreta @import, url()... 
+            ]
+        }]
     }
 
 }
